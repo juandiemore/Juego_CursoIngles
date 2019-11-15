9fdea0bca74d007ab67eb3e87c4e69b1be4df26f
juan <juandiemore@gmail.com>
2019-11-14 21:43:23 -0500

    adding life2

---
+++ CursoIngles/Assets/Scripts/controlVida.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlVida : MonoBehaviour
+{   
+    private Animator anim;
+    private int vida = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(vida <)   
+    }
+
+        
+    void GetVida(){
+        
+    }
+
+        
+    private void OnTriggerEnter(Collider other) {
+    }
+}
